2d4ce20ff085972be9fa4a2e2ae32d7360e892da
Phirun Tara <taraphirun@live.com>
2025-12-25 19:02:08 +0700

    refactor: update custom fields for after_install function

--- klik_pos/install.py
+++ klik_pos/install.py
@@ -5,16 +5,6 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 def after_install():
 	"""Create custom fields after app installation"""
 	create_custom_fields({
-		"POS Invoice Item": [
-			{
-				"fieldname": "custom_ds_roofing_spec",
-				"label": "Roofing Spec",
-				"fieldtype": "JSON",
-				"default": "[]",
-				"read_only": 0,
-				"insert_after": "item_group",
-			},
-		],
 		"Sales Invoice Item": [
 			{
 				"fieldname": "custom_ds_roofing_spec",
